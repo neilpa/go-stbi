7834dea13311ebfe9e0bef7d2d4b2933d5eb9309
Neil Pankey <npankey@gmail.com>
2019-12-07 13:27:34 -0800

    stbi: Simple stb_image.h wrapper

---
+++ README.md
@@ -0,0 +1,5 @@
+# go-stbi
+
+Go binding for [stb_image.h][]
+
+[stb_image.h]: https://github.com/nothings/stb/blob/f67165c2bb2af3060ecae7d20d6f731173485ad0/stb_image.h
---
+++ stbi.go
@@ -0,0 +1,33 @@
+package stbi
+
+import (
+	"errors"
+	"image"
+	"unsafe"
+)
+
+// #cgo LDFLAGS: -lm
+// #define STB_IMAGE_IMPLEMENTATION
+// #include "stb_image.h"
+// #include <stdlib.h>
+import "C"
+
+// Load reads an image from disk into memory as RGBA pixel format.
+func Load(path string) (*image.RGBA, error) {
+	cpath := C.CString(path)
+	defer C.free(unsafe.Pointer(cpath))
+
+	var x, y C.int
+	data := C.stbi_load(cpath, &x, &y, nil, 4)
+	if data == nil {
+		msg := C.GoString(C.stbi_failure_reason())
+		return nil, errors.New(msg)
+	}
+	defer C.stbi_image_free(unsafe.Pointer(data))
+
+	return &image.RGBA{
+		Pix:	C.GoBytes(unsafe.Pointer(data), y*x*4),
+		Stride: 4,
+		Rect:   image.Rect(0, 0, int(x), int(y)),
+	}, nil
+}
